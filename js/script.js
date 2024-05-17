50dec4e39848ad3dd650cb55818139e55198a533
Josh Kim <joshuajk@al.inpser.edu.br>
2024-05-17 13:37:07 -0300

    Java implementado

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded',function() {
+    buttons = document.querySelectorAll('.button')
+    filename = location.href.split("/").slice(-1)
     bar = document.querySelector('.progress')
-    progresso = localStorage.getItem('progresso')
-    if (progresso === null || progresso === 0) {
+    progresso = localStorage.getItem('progresso'+filename)
+    if (progresso === null) {
         progresso = 0
-        bar.style.width = progresso + '%'
-        bar.innerHTML = progresso + '%'
+    }
+    bar.style.width = progresso + '%'
+    bar.innerHTML = progresso+'%'
+    
+    for (button of buttons) {
+        button.addEventListener('click', function(event) {
+            t = event.currentTarget
+            if (progresso < 100) {
+                progresso += 1
+                localStorage.setItem('progresso'+filename,progresso)
+                bar.style.width = progresso + '%'
+                bar.innerHTML = progresso + '%'
+            }
+            event.preventDefault()
+        })
     }
 })
